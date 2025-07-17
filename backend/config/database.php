c93a5def043f3f231b4e288fb0e6541469be2c83
Thierno Ngom <thierno.tngom1010@gmail.com>
2025-07-17 21:03:26 +0000

    🔧 Corrige .env pour Render + mise à jour de la config de production

--- backend/config/database.php
+++ backend/config/database.php
@@ -35,16 +35,10 @@ return [
 
     'connections' => [
 
-        'mongodb' => [
-            'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'pointage_db'),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
-            'options' => [
-                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
-            ],
+                'mongodb' => [
+                    'driver' => 'mongodb',
+                    'dsn' => env('DB_DSN'),
+                    'database' => env('DB_DATABASE'),
         ],
 
       
